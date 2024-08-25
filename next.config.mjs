dae8c428543690b8dfb334a01e4d627fc025e930
Debayan Pradhan <pradhandebayan@gmail.com>
2024-08-26 02:12:16 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+import nextPWA from '@ducanh2912/next-pwa';
+
+const withPWA = nextPWA({
+  dest: "public",
+  cacheOnFrontEndNav:true,
+  aggressiveFrontEndNavCaching:true,
+  reloadOnOnline:true,
+  swcMinify:true,
+  disable:false,
+  workboxOptions: {
+    disableDevLogs:true,
+  }
+});
+
+
+export default withPWA(nextConfig);
